fea275e63b9ab2139d4502f7a37c447e6134935f
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2023-08-20 19:14:56 +0530

    Updated sandbox

--- flutter_basic_projects/lib/Animated_widgets/sandbox.dart
+++ flutter_basic_projects/lib/Animated_widgets/sandbox.dart
@@ -16,10 +16,13 @@ class Sandbox extends StatefulWidget
   State<Sandbox> createState() => _SandboxState();
 }
 
+
 class _SandboxState extends State<Sandbox> 
 {
-  double width = 10;
-  double margin = 0;
+  double _opacity = 1;
+  Color _color = Colors.blue;
+  double _width = 200;
+  double _margin = 0;
 
   @override
   Widget build(BuildContext context) 
@@ -27,14 +30,60 @@ class _SandboxState extends State<Sandbox>
     return MaterialApp(
       home: Scaffold(
         body: AnimatedContainer(
+        margin: EdgeInsets.all(_margin),
+        width: _width,
+        color: _color,
         duration: Duration(milliseconds: 900),
+
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+
           children: [
+            const SizedBox(height: 50,),
+
             ElevatedButton(
             onPressed: (){
-              width = 20;
+              setState(() {
+                _margin = 50;
+              });
             },
-            child: const Text("Animate Size"))
+            child: const Text("Animate Margin")),
+
+             const SizedBox(height: 50,),
+
+            ElevatedButton(
+            onPressed: (){
+              setState(() {
+                _color = Colors.amber;
+              });
+            },
+            child: const Text("Animate Color")),
+
+             const SizedBox(height: 50,),
+
+            ElevatedButton(
+            onPressed: (){
+              setState(() {
+                _width = 300;
+              });
+            },
+            child: const Text("Animate width")),
+
+             const SizedBox(height: 50,),
+
+             ElevatedButton(
+            onPressed: (){
+              setState(() {
+                _opacity = 0;
+              });
+            },
+            child: const Text("Animate Opacity")),
+
+           AnimatedOpacity(
+           opacity: _opacity,
+           child: Text('Hide Me',style: TextStyle(color:Colors.white),),
+           duration: Duration(seconds: 2))
+            
           ],
         ),
         ),
