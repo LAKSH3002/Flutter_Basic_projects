bcb284037bb325fa174c42f195412b63554bfca5
Laksh <lakshd1999@gmail.com>
2023-10-17 22:28:23 +0530

    Something added

---
+++ flutter_basic_projects/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\laksh doshi\\Downloads\\flutter_windows_3.7.8-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\laksh doshi\\Downloads\\flutter_windows_3.7.8-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects"
+  "FLUTTER_ROOT=C:\\Users\\laksh doshi\\Downloads\\flutter_windows_3.7.8-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects"
+  "FLUTTER_TARGET=C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects\\lib\\widgets\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\laksh doshi\\OneDrive\\Desktop\\Flutter_Basic_projects\\flutter_basic_projects\\.dart_tool\\package_config.json"
+)
